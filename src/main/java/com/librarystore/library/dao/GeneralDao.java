16a98eab7dd2f3c441be990164f4d820b6e26590
arma73 <arma.biz9805@gmail.com>
2020-06-27 21:51:39 +0400

    generic interfaces for describing specific behavior for working with Entity objects

---
+++ src/main/java/com/librarystore/library/dao/GeneralDao.java
@@ -0,0 +1,17 @@
+package com.librarystore.library.dao;
+
+import java.util.List;
+
+/**
+ * @param <E> the type of element for access data
+ * @see AuthorDao
+ * @see BookDao
+ * @see GenreDao
+ * @see PublisherDao
+ */
+public interface GeneralDao<E> {
+    List<E> getAll();
+    E get(long id);
+    E save(E entity);
+    void delete(E entity);
+}
