9f8cc7c5846ed97cca834ee1843890087bb07bf5
arma73 <arma.biz9805@gmail.com>
2020-06-28 02:50:13 +0400

    add dao method for author. Get the list of authors page by page

--- src/main/java/com/librarystore/library/spring/repository/AuthorRepository.java
+++ src/main/java/com/librarystore/library/spring/repository/AuthorRepository.java
@@ -1,6 +1,8 @@
 package com.librarystore.library.spring.repository;
 
 import com.librarystore.library.domain.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,11 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
      * @param fio name sent from client
      */
     List<Author> findByFioContainingIgnoreCaseOrderByFio(String fio);
+
+    /**
+     * Get the list of authors page by page
+     * @param fio name sent from client
+     * @param pageable parameters for page. Implement with {@link org.springframework.data.domain.PageRequest}
+     */
+    Page<Author> findByFioContainingIgnoreCaseOrderByFio(String fio, Pageable pageable);
 }
