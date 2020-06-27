8755757b9b2b178ba64a51d13f260bbd85bf67ed
arma73 <arma.biz9805@gmail.com>
2020-06-27 20:35:48 +0400

    make entity class Book

--- src/main/java/com/librarystore/library/domain/Book.java
+++ src/main/java/com/librarystore/library/domain/Book.java
@@ -8,7 +8,13 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.SelectBeforeUpdate;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Lob;
+import javax.persistence.Column;
+
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(catalog = "library")
@@ -18,10 +24,42 @@ import javax.persistence.Table;
 @EqualsAndHashCode(of = "id")
 @Setter @Getter
 public class Book {
-    public Book() {
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Book(Long id, byte[] image) {
+    private String name;
 
+    @Lob
+    @Column(updatable = false)
+    private byte[] content;
+
+    @Column(name = "page_count")
+    private Integer pageCount;
+
+    private String isbn;
+
+    @Column(name = "publish_year")
+    private Integer publishYear;
+
+    private byte[] image;
+
+    private String descr;
+
+    @Column(name = "view_count")
+    private long viewCount;
+
+    @Column(name = "total_rating")
+    private long totalRating;
+
+    @Column(name = "total_vote_count")
+    private long totalVoteCount;
+
+    @Column(name = "avg_rating")
+    private int avgRating;
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
