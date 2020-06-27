e7d8f3630ee94a24b8d989ecca89505b2ab43f4a
arma73 <arma.biz9805@gmail.com>
2020-06-27 22:02:38 +0400

    Adding abstract methods to find top books

--- src/main/java/com/librarystore/library/dao/BookDao.java
+++ src/main/java/com/librarystore/library/dao/BookDao.java
@@ -2,7 +2,21 @@ package com.librarystore.library.dao;
 
 import com.librarystore.library.domain.Book;
 
+import java.util.List;
+
 /**
  *  Describes specific behavior for working with books.
  */
-public interface BookDao extends GeneralDao<Book> { }
+public interface BookDao extends GeneralDao<Book> {
+    /**
+     * Search for Top Books
+     * @param limit maximum number of books
+     */
+    List<Book> findTopBooks(int limit);
+    /**
+     * Search for Top Books
+     * @param begin starting point
+     * @param limit maximum number of books from the starting point
+     */
+    List<Book> findTopBooks(int begin, int limit);
+}
