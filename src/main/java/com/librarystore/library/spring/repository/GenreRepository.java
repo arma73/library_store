717b4a98283961478bbf4f5269388b6391cdac72
arma73 <arma.biz9805@gmail.com>
2020-06-28 11:47:04 +0400

    receive genres and publications page by page

--- src/main/java/com/librarystore/library/spring/repository/GenreRepository.java
+++ src/main/java/com/librarystore/library/spring/repository/GenreRepository.java
@@ -1,6 +1,8 @@
 package com.librarystore.library.spring.repository;
 
 import com.librarystore.library.domain.Genre;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,10 @@ import java.util.List;
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
     List<Genre> findByNameContainingIgnoreCaseOrderByName(String name);
+
+    /**
+     * Get the list of genres page by page
+     * @param pageable parameters for page. Implement with {@link org.springframework.data.domain.PageRequest}
+     */
+    Page<Genre> findByNameContainingIgnoreCaseOrderByName(String name, Pageable pageable);
 }
--- src/main/java/com/librarystore/library/spring/repository/PublisherRepository.java
+++ src/main/java/com/librarystore/library/spring/repository/PublisherRepository.java
@@ -1,6 +1,8 @@
 package com.librarystore.library.spring.repository;
 
 import com.librarystore.library.domain.Publisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,10 @@ import java.util.List;
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
     List<Publisher> findByNameContainingIgnoreCaseOrderByName(String name);
+
+    /**
+     * Get the list of publishers page by page
+     * @param pageable parameters for page. Implement with {@link org.springframework.data.domain.PageRequest}
+     */
+    Page<Publisher> findByNameContainingIgnoreCaseOrderByName(String name, Pageable pageable);
 }
